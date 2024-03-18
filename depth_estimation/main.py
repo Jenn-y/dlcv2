d717414540a40c3b00a9bb67c69b066cf7f9316f
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2024-03-18 06:16:53 +0100

    depth setup

---
+++ depth_estimation/main.py
@@ -0,0 +1,2 @@
+def get_depth_map(self, image):
+    """Get depth map from image and depth"""
---
+++ main.py
@@ -0,0 +1,4 @@
+from depth_estimation.main import get_depth_map
+
+image = 'path/to/image.jpg'
+depth_map = get_depth_map(image)
